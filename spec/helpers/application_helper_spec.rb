0a8b50ee214eccf0325ea447469dfe79f07ac3ab
inada <starmelt190@gmail.com>
2020-05-05 16:21:37 +0900

    helper_specにて無駄なcontextを削除しました。

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,22 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "ApplicationHelperに関するhelperテスト", type: :helper do
   describe "#full_titleメソッドに関するテスト" do
-    context "ページタイトルが空のとき" do
-      it "ページタイトルが空欄の時テスト" do
-        expect(helper.full_title(' ')).to eq('BIGBAG Store')
-      end
+    it "ページタイトルが空欄の時テスト" do
+      expect(helper.full_title(' ')).to eq('BIGBAG Store')
     end
 
-    context "ページタイトルがnilのとき" do
-      it "ページタイトルがnilの時テスト" do
-        expect(helper.full_title(nil)).to eq('BIGBAG Store')
-      end
+    it "ページタイトルがnilの時テスト" do
+      expect(helper.full_title(nil)).to eq('BIGBAG Store')
     end
 
-    context "ページタイトルが入っているとき" do
-      it "フルタイトルメソッドのテスト(titleはTestと代入)" do
-        expect(helper.full_title('Test')).to eq('Test - BIGBAG Store')
-      end
+    it "ページタイトルが入っている時のテスト(titleはTestと代入)" do
+      expect(helper.full_title('Test')).to eq('Test - BIGBAG Store')
     end
   end
 end
